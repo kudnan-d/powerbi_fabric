9d893efb939dcf277c8bf0bd2b824f12dc9c9b1d
Kundan D <kundandombale@outlook.com>
2025-08-28 10:25:15 +0530

    Committing 5 items from workspace ff7400ee-394f-4119-bd29-36a94744b727

---
+++ Hello World.Notebook/notebook-content.py
@@ -0,0 +1,35 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "47dc3d49-cfb5-419c-9e36-ab95a0421730",
+# META       "default_lakehouse_name": "bronze_lh",
+# META       "default_lakehouse_workspace_id": "ff7400ee-394f-4119-bd29-36a94744b727",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "47dc3d49-cfb5-419c-9e36-ab95a0421730"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+print("hello from feature 2")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
